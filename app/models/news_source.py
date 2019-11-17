8a9d2eff38d3c5b602ee8615281f826716cf3d57
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-17 06:23:54 +0300

    create macros.html and extend it to index.html

---
+++ app/models/news_source.py
@@ -0,0 +1,16 @@
+class Source:
+
+    '''
+    class to define source objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country):
+
+        self.id=id
+        self.name=name
+        self.description=description
+        self.url="http://www.bbc.co.uk/news"
+        self.category=category
+        self.country=country
+
+        
