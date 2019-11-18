d565614a5bd85ebfbdf2e92c154b1940322bfc0f
Nyakinyua <wanyakinyua968@gmail.com>
2019-11-18 21:15:55 +0300

    use @manager.command decorator to create a new command

--- tests/article_test.py
+++ tests/article_test.py
@@ -9,4 +9,8 @@ class ArticlesTest(unittest.TestCase):
         '''
         set up method that will run before every Test
         '''
-        self.new_article=Articles((id,name,author,title,description,url,urlToImage,publishedAt,content))
+        self.new_article=Articles((32,"Abc news","T.W.Jones","Review","Frozen 2 soundtrack struggles in shadow of first","https://www.movie-zone.com","https:www.BMW.com/wecc/3fcx/wsx.jpg","23-Jan-2019",'Various artists, Frozen 2')
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
